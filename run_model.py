ba27a75698a9241ee570b697114b27598815bda0
hanuel <ezzeezz@naver.com>
2022-12-06 16:03:34 +0900

    feat: Flask server to server model

---
+++ run_model.py
@@ -0,0 +1,246 @@
+# Import packages
+from typing import List, Dict
+import tqdm.notebook as tq
+from tqdm.notebook import tqdm
+import json
+import pandas as pd
+import numpy as np
+from tokenizer import tokenizer
+
+import torch
+from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from transformers import (
+    AdamW,
+    T5ForConditionalGeneration,
+    T5TokenizerFast as T5Tokenizer
+    )
+
+from flask import Flask, jsonify, request
+
+
+SEP_TOKEN = ''
+MASKING_CHANCE = 0.3 #30% chance to replace the answer with '[MASK]'
+
+
+class QGDataset(Dataset):
+
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        tokenizer: T5Tokenizer,
+        source_max_token_len: int,
+        target_max_token_len: int
+        ):
+
+        self.tokenizer = tokenizer
+        self.data = data
+        self.source_max_token_len = source_max_token_len
+        self.target_max_token_len = target_max_token_len
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index: int):
+        data_row = self.data.iloc[index]
+
+        if np.random.rand() > MASKING_CHANCE:
+            answer = data_row['answer_text']
+        else:
+            answer = '[MASK]'
+
+        source_encoding = tokenizer(
+            '{} {} {}'.format(answer, SEP_TOKEN, data_row['context']),
+            max_length= self.source_max_token_len,
+            padding='max_length',
+            truncation= True,
+            return_attention_mask=True,
+            add_special_tokens=True,
+            return_tensors='pt'
+            )
+    
+        target_encoding = tokenizer(
+            '{} {} {}'.format(data_row['answer_text'], SEP_TOKEN, data_row['question']),
+            max_length=self.target_max_token_len,
+            padding='max_length',
+            truncation = True,
+            return_attention_mask=True,
+            add_special_tokens=True,
+            return_tensors='pt'
+            )
+
+        labels = target_encoding['input_ids']  
+        labels[labels == 0] = -100
+
+        return dict(
+            answer_text = data_row['answer_text'],
+            context = data_row['context'],
+            question = data_row['question'],
+            input_ids = source_encoding['input_ids'].flatten(),
+            attention_mask = source_encoding['attention_mask'].flatten(),
+            labels=labels.flatten()
+            )
+    
+    
+class QGDataModule(pl.LightningDataModule):
+
+    def __init__(
+        self,
+        train_df: pd.DataFrame,
+        val_df: pd.DataFrame,
+        test_df: pd.DataFrame,
+        tokenizer: T5Tokenizer,
+        batch_size,
+        source_max_token_len: int,
+        target_max_token_len: int
+        ): 
+        super().__init__()
+        self.batch_size = batch_size
+        self.train_df = train_df
+        self.val_df = val_df
+        self.test_df = test_df
+        self.tokenizer = tokenizer
+        self.source_max_token_len = source_max_token_len
+        self.target_max_token_len = target_max_token_len
+
+    def setup(self):
+        self.train_dataset = QGDataset(self.train_df, self.tokenizer, self.source_max_token_len, self.target_max_token_len)
+        self.val_dataset = QGDataset(self.val_df, self.tokenizer, self.source_max_token_len, self.target_max_token_len)
+        self.test_dataset = QGDataset(self.test_df, self.tokenizer, self.source_max_token_len, self.target_max_token_len)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size = self.batch_size, shuffle=True, num_workers = 2)
+
+    def val_dataloader(self): 
+        return DataLoader(self.val_dataset, batch_size=1, num_workers=2)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=1, num_workers=2)
+      
+
+MODEL_NAME = 'paust/pko-t5-small'
+SOURCE_MAX_TOKEN_LEN = 300
+TARGET_MAX_TOKEN_LEN = 80
+
+N_EPOCHS = 5
+BATCH_SIZE = 4
+LEARNING_RATE = 0.0001
+
+
+tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
+print('tokenizer len before: ', len(tokenizer))
+tokenizer.add_tokens(SEP_TOKEN)
+print('tokenizer len after: ', len(tokenizer))
+TOKENIZER_LEN = len(tokenizer)
+
+
+class QGModel(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME, return_dict=True)
+        self.model.resize_token_embeddings(TOKENIZER_LEN) #resizing after adding new tokens to the tokenizer
+
+    def forward(self, input_ids, attention_mask, labels=None):
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+        return output.loss, output.logits
+
+    def training_step(self, batch, batch_idx):
+        input_ids = batch['input_ids']
+        attention_mask = batch['attention_mask']
+        labels = batch['labels']
+        loss, output = self(input_ids, attention_mask, labels)
+        self.log('train_loss', loss, prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        input_ids = batch['input_ids']
+        attention_mask = batch['attention_mask']
+        labels = batch['labels']
+        loss, output = self(input_ids, attention_mask, labels)
+        self.log('val_loss', loss, prog_bar=True, logger=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        input_ids = batch['input_ids']
+        attention_mask = batch['attention_mask']
+        labels = batch['labels']
+        loss, output = self(input_ids, attention_mask, labels)
+        self.log('test_loss', loss, prog_bar=True, logger=True)
+        return loss
+  
+    def configure_optimizers(self):
+        return AdamW(self.parameters(), lr=LEARNING_RATE)
+      
+
+checkpoint_path = '/Users/ihaneul/desktop/aiback/best-checkpoint-v6.ckpt'
+
+best_model = QGModel.load_from_checkpoint(checkpoint_path)
+best_model.freeze()
+best_model.eval()
+
+
+def generate(qgmodel: QGModel, answer: str, context: str) -> str:
+    source_encoding = tokenizer(
+        '{} {} {}'.format(SEP_TOKEN, answer, context),
+        max_length=SOURCE_MAX_TOKEN_LEN,
+        padding='max_length',
+        truncation=True,
+        return_attention_mask=True,
+        add_special_tokens=True,
+        return_tensors='pt'
+    )
+
+    generated_ids = qgmodel.model.generate(
+        input_ids=source_encoding['input_ids'],
+        attention_mask=source_encoding['attention_mask'],
+        num_beams=1,
+        max_length=TARGET_MAX_TOKEN_LEN,
+        repetition_penalty=1.0,
+        length_penalty=1.0,
+        early_stopping=True,
+        use_cache=True
+    )
+
+    preds = {
+        tokenizer.decode(generated_id, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+        for generated_id in generated_ids
+    }
+
+    return ''.join(preds)
+
+  
+def show_result(generated: str):
+    print('Generated: ', generated)
+    
+app = Flask(__name__)
+
+
+
+
+@app.route("/ai", methods=["POST"])
+def index():
+    input_answer = '[MASK]'
+    params = request.get_json()
+
+    context1 = params['text1']
+    context2 = params['text2']
+    context3 = params['text3']
+    context4 = params['text4']
+    context5 = params['text5']
+    context6 = params['text6']
+    context7 = params['text7']
+
+    generated1 = generate(best_model, input_answer, context1).split("  ")[1]
+    generated2 = generate(best_model, input_answer, context2).split("  ")[1]
+    generated3 = generate(best_model, input_answer, context3).split("  ")[1]
+    generated4 = generate(best_model, input_answer, context4).split("  ")[1]
+    generated5 = generate(best_model, input_answer, context5).split("  ")[1]
+    generated6 = generate(best_model, input_answer, context6).split("  ")[1]
+    generated7 = generate(best_model, input_answer, context7).split("  ")[1]
+
+    return jsonify({"text1":generated1, "text2":generated2, "text3":generated3, "text4":generated4, "text5":generated5, "text6":generated6, "text7":generated7})
+
+if __name__ == "__name__":
+    app.run()
